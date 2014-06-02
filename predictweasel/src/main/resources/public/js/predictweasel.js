1cca9e6311f48d9557e7d9b1a2252585c2628a17
Andrew Gillies <andrew@leastweasel.org>
2014-06-02 11:21:43 +0100

    Use meta tag to decide which menu item to select.
    Define a meta tag with name 'active-menu' and the menu item identified
    by the content attribute will be made the only active item,

--- predictweasel/src/main/resources/public/js/predictweasel.js
+++ predictweasel/src/main/resources/public/js/predictweasel.js
@@ -1,3 +1,15 @@
+/**
+ * This event handler is called to signify that the page is ready as the DOM
+ * has been fully initialised.
+ */
+$(document).ready(function() {
+	var activeMenu = $('meta[name=active-menu]').attr("content");
+	
+	if (activeMenu) {
+		selectNavMenu(activeMenu);
+	}
+});
+
 /*
  * Select (by giving it an extra "active" class) the menu item identified
  * by the given jQuery selector.
