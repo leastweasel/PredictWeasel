b663092cd7726be189c61aecc3b2c058589ef3ec
Andrew Gillies <andrew@leastweasel.org>
2014-06-04 21:33:16 +0100

    Add standard methods to PlayerStanding
    Add standard methods (equals, hashCode, compareTo) to PlayerStanding so
    we can display them in order.

--- predictweasel/src/main/java/org/leastweasel/predict/domain/PlayerStanding.java
+++ predictweasel/src/main/java/org/leastweasel/predict/domain/PlayerStanding.java
@@ -4,12 +4,18 @@
  */
 package org.leastweasel.predict.domain;
 
+import org.apache.commons.lang.builder.CompareToBuilder;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
 /**
  * Where a player's score stands within a {@link Prize}. These are calculated on demand rather than
- * being persisted to the database (they can be cached for increaed performance as they only change
- * when rew results are entered).
+ * being persisted to the database (they can be cached for increased performance as they only change
+ * when new results are entered).
+ * <p>
+ * Player standings sort naturally on the number of points scored, then by player name.
  */
-public class PlayerStanding {
+public class PlayerStanding implements Comparable<PlayerStanding> {
 	private final User player;
 	
 	private int position;
@@ -75,5 +81,60 @@ public class PlayerStanding {
 	public int getPointsScored() {
 		return pointsScored;
 	}
+
+    /**
+     * Equality operator.
+     *
+     * @param other the object we're comparing against
+     * @return true if the objects are equal
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (other == null) {
+            return false;
+        }
+
+        if (!(other instanceof PlayerStanding)) {
+            return false;
+        }
+
+        final PlayerStanding standing = (PlayerStanding) other;
+
+        return new EqualsBuilder().append(player.getName(), standing.player.getName())
+        							 .append(pointsScored, standing.pointsScored)
+                                  .isEquals();
+    }
+
+    /**
+     * Generate a hash code for this object.
+     *
+     * @return a hash code
+     */
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(player.getName())
+        							   .append(pointsScored)
+                                    .toHashCode();
+    }
+    
+    /**
+     * Compare two instances. Note that the comparison is done back to front
+     * for what is considered normal because we want the highest point scores
+     * to appear first in the list. But we want names to sort normally when
+     * the scores are tied.
+     * 
+     * @param o the other player standing to compare
+     * @return the usual result for a comparison, but reversed
+     */
+    @Override
+    public int compareTo(PlayerStanding o) {
+	    	return new CompareToBuilder().append(o.pointsScored, pointsScored)
+	    								.append(player.getName(), o.player.getName())
+	    								.toComparison();
+    }
 }
 
